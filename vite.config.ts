ca65f8f0ff5d1863a4ed9ad4f0eafdbcd2014311
Jazz877 <jazz@kleomed.es>
2024-09-06 07:22:52 +0000

    fix(polyfill): remove vite-plugin-node-polyfills, use manual setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), nodePolyfills()],
+  define: {
+    global: "window",
+  },
+  plugins: [react(), viteTsconfigPaths()],
   server: {
     open: true,
     host: true,
