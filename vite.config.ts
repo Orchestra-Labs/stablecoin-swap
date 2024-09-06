407ce4de7134498c636c7fe767741abb7d14d4ca
cmancrypto <104414667+cmancrypto@users.noreply.github.com>
2024-09-06 13:53:56 +1000

    fix: vite config path resolver

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,15 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path'
 
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths(), nodePolyfills()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     open: true,
     host: true,
