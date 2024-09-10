61f7222d70651fda1e570bebf07042cba430bf64
Jazz877 <107574486+Jazz877@users.noreply.github.com>
2024-09-10 15:12:28 +0200

    fix: solve rendering issue and use calculateReceiveAmount function for receiveAmount "prediction" (#17)
    Co-authored-by: cmancrypto <104414667+cmancrypto@users.noreply.github.com>

--- src/sections/SwapSection/hooks/useReceiveAmount.ts
+++ src/sections/SwapSection/hooks/useReceiveAmount.ts
@@ -2,13 +2,17 @@ import { useAtomValue } from 'jotai';
 
 import { useExchangeRate } from '@/hooks';
 import { SendAmountAtom } from '@/sections/SwapSection/atoms/SendAmountAtom';
+import { calculateReceiveAmount } from '../utils/swapCalculation';
+import BigNumber from 'bignumber.js';
 
 export const useReceiveAmount = () => {
   const { exchangeRate, isLoading, error } = useExchangeRate();
   const sendAmount = useAtomValue(SendAmountAtom);
 
+  const receiveAmount = calculateReceiveAmount(sendAmount, BigNumber(exchangeRate)).toNumber();
+
   return {
-    receiveAmount: sendAmount * exchangeRate,
+    receiveAmount: receiveAmount,
     isLoading,
     error,
   };
