6f1fb89e894533c05a09cdcc5b5f861a0a8da333
David Blackstone <dblackstone@dblackstone.me>
2025-10-11 14:02:58 -0400

    fix: amend polyfill file

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -1,4 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Buffer } from 'buffer';
+import process from 'process';
 
 (window as any).Buffer = Buffer;
+(window as any).process = process;
+
+// Polyfill for process in global scope
+if (typeof global.process === 'undefined') {
+  (global as any).process = process;
+}
